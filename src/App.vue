98a96fbb285e22a2c67237955a53891cdf473c85
wilf_wang <wilf_wang@digifinex.org>
2022-09-13 18:29:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Login from "./components/Login.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Letter</h1>
+
+    <Login/>
+  </div>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
