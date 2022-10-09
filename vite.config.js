da0caa2fd419c7c3a79f02f0c1d254980b3f6995
wilf_wang <wilf_wang@digifinex.org>
2022-10-09 17:48:03 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { join } from "path";
+import { posix } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,7 +28,7 @@ export default defineConfig({
   },
   resolve: {
       alias: {
-          "@": join(__dirname, "src"),
+          "@": posix.join(__dirname, "src"),
       }
   }
 });
